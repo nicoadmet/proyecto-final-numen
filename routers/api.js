9d4329e273c487c4a40459f50ea8db563ae72348
nicoadmet <admetnico@gmail.com>
2023-11-13 17:24:45 -0300

    cambie URN de las rutas

--- routers/api.js
+++ routers/api.js
@@ -3,10 +3,10 @@ const router = express.Router(); //segmentar una ruta
 const apiController = require('../controllers/apiController');
 
 
-router.get('/datos', apiController.getPrisoners)
+router.get('/', apiController.getPrisoners)
 router.post('/crear', apiController.postPrisoners)
-router.put('/actualizar', apiController.putPrisoners)
-router.delete('/eliminar', apiController.deletePrisoners)
+router.put('/:id', apiController.putPrisoners)
+router.delete('/:id', apiController.deletePrisoners)
 
 
 
