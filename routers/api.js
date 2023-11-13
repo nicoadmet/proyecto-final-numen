610a6265d48f637a97b0e4f05f023826a77e3561
nicoadmet <admetnico@gmail.com>
2023-11-13 18:21:54 -0300

    actualicé middlewares

--- routers/api.js
+++ routers/api.js
@@ -10,7 +10,9 @@ router.get('/', apiController.getPrisoners)
 router.post('/crear',
 [
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
-    check("nroPreso", "El número de preso debe tener mínimo 4 caracteres ").isLength({min:4}),
+    check("nroPreso", "El número de preso debe tener mínimo 4 caracteres ").isLength({
+        min:4,
+    }),
     check("nroPreso").custom(presoExiste),
     validarCampos,
 
